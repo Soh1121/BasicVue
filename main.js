704d10a3eb598c7b239572cebc778066898df42b
Soh1121 <satou.shg@gmail.com>
2019-05-01 15:58:41 +0900

    [update]テキストのデータバインディングの確認

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-var app = new VTTCue({
-    el: '#app'
+var app = new Vue({
+    el: '#app',
+    data: {
+        // オブジェクトデータ
+        message: {
+            value: 'Hello Vue.js!'
+        },
+        // 配列データ
+        list: ['りんご', 'ばなな', 'いちご'],
+        // 別のデータを使用してlistから取り出す要素を動的に
+        num: 1
+    }
 })
