a2b3ed2e2f7c185f820932be4a4f6692a331267d
Soh1121 <satou.shg@gmail.com>
2019-05-01 17:14:53 +0900

    [update]リスト要素を削除
    ボタンをクリックして要素を削除

--- main.js
+++ main.js
@@ -21,6 +21,12 @@ var app = new Vue({
                 name: this.name,    // 現在のフォームの入力値
                 hp: 500
             })
+        },
+
+        // 削除ボタンをクリックしたときのハンドラ
+        doRemove: function(index) {
+            // 受け取ったインデックスの位置から1個要素を削除
+            this.list.splice(index, 1)
         }
     }
 })
