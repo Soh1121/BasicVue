b16c21d7312f2f48128fc8c27255aa39fa7021cd
Soh1121 <satou.shg@gmail.com>
2019-05-01 21:44:14 +0900

    [update]外部からデータを取得するサンプル
    外部からjsonファイルを取得して描画

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name: 'キマイラ',
-        list: [
-            { id: 1, name: 'スライム', hp: 100 },
-            { id: 2, name: 'ゴブリン', hp: 200 },
-            { id: 3, name: 'ドラゴン', hp: 500 }
-        ]
+        // あらかじめ空リストを用意しておく
+        list: []
     },
-    methods: {
-        // 追加ボタンをクリックしたときのハンドラ
-        doAdd: function() {
-            // リスト内で1番大きいIDを取得
-            var max = this.list.reduce(function(a, b) {
-                return a > b.id ? a : b.id
-            }, 0)
-            // 新しいモンスターをリストに追加
-            this.list.push({
-                id: max + 1,    // 現在の最大のIDに+1してユニークなIDを作成
-                name: this.name,    // 現在のフォームの入力値
-                hp: 500
-            })
-        },
-
-        // 削除ボタンをクリックしたときのハンドラ
-        doRemove: function(index) {
-            // 受け取ったインデックスの位置から1個要素を削除
-            this.list.splice(index, 1)
-        },
-
-        // ボタンのクリックイベントのハンドラ
-        doAttack: function(index) {
-            this.list[index].hp -= 10   // HPを減らす
-        }
+    created: function() {
+        axios.get('list.json').then(function(response) {
+            // 取得完了したらlistリストに代入
+            this.list = response.data
+        }.bind(this)).catch(function(e) {
+            console.error(e)
+        })
     }
 })
