96e7c064180f979bbff275563a52dabe5acfca1e
Soh1121 <satou.shg@gmail.com>
2019-05-01 16:21:18 +0900

    [update]SVGのデータバインディングの確認
    フォームのスライダーの数値と同期してSVGの円の半径が変化

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        count: 0
-    },
-    methods: {
-        // ボタンをクリックしたときのハンドラ
-        increment: function() {
-            this.count += 1 // 処理は再代入するだけでOK！
-        }
+        radius: 50
     }
 })
