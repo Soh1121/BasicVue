5c79981a19af3ca3b021d116765fe1b8c81f9aff
Soh1121 <satou.shg@gmail.com>
2019-05-02 21:51:09 +0900

    [update]DOM操作してもデータに変化があれば仮想DOMによって上書き
    「カウントアップ」ボタンでカウントを増やしても、`v-if`の切り替えを行うと元の「0」に戻ることの確認

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var app = new Vue({
     el: '#app',
-    mounted: function() {
-        console.log(this.$refs.hello)
+    data: {
+        show: true
+    },
+    methods: {
+        handleClick() {
+            var count = this.$refs.count
+            if (count) {
+                count.innerText = parseInt(count.innerText, 10) + 1
+            }
+        }
     }
 })
