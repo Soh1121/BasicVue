432db4332acb08ab2f434db3290d12659091e63b
Soh1121 <satou.shg@gmail.com>
2019-05-02 22:01:21 +0900

    [update]v-onceの確認
    1回だけmessageの内容を表示してリアクティブを解除

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        show: true
-    },
-    methods: {
-        handleClick() {
-            var count = this.$refs.count
-            if (count) {
-                count.innerText = parseInt(count.innerText, 10) + 1
-            }
-        }
+        url: "https://jp.vuejs.org/",
+        message: " Vue.js!"
     }
 })
