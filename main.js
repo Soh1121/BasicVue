0aed746d2de4477b3da4dc2849a29f7e2febd6ca
Soh1121 <satou.shg@gmail.com>
2019-05-07 21:52:43 +0900

    [update]v-modelの使い方
    フォームの文字を編集すると同期して瞬時に画面を更新

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 var app = new Vue({
     el: '#app',
-    methods: {
-        handler: function(comment) {
-            // 代入前になにか処理を行う
-            console.log(comment)
-        }
+    data: {
+        message: 'Hello!'
     }
 })
