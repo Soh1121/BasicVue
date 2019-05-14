a0ac8fc8acae071b48afd8eeea1807992098ad09
Soh1121 <satou.shg@gmail.com>
2019-05-14 21:17:45 +0900

    [update]算出プロパティを組み合わせて使う
    width × heightの中心座標をオブジェクトで返す

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ var scroll = new SmoothScroll()
 var app = new Vue({
     el: '#app',
     data: {
-        width: 800
+        width: 800,
+        height: 600
     },
     computed: {
-        // 算出プロパティhalfWidthを定義
         halfWidth: function() {
             return this.width / 2
+        },
+        halfHeight: function() {
+            return this.height / 2
+        },
+        // 「width × height」の中心座標をオブジェクトで返す
+        halfPoint: function() {
+            return {
+                x: this.halfWidth,
+                y: this.halfHeight
+            }
         }
     }
 })
