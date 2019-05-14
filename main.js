f71313ccac47374eeeb7b5f65622811c14aa2739
Soh1121 <satou.shg@gmail.com>
2019-05-14 21:32:41 +0900

    [update]算出プロパティのキャッシュ機能
    random関数を用いてキャッシュを確認

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 var scroll = new SmoothScroll()
 var app = new Vue({
     el: '#app',
-    data: {
-        width: 800
-    },
     computed: {
-        halfWidth: {
-            get: function() { return this.width / 2 },
-            // halfWidthの2倍の数値をwidthに代入する
-            set: function(val) { this.width = val * 2 },
-        }
+        computedData: function() { return Math.random() }
+    },
+    methods: {
+        methodsData: function() { return Math.random() }
     }
 })
